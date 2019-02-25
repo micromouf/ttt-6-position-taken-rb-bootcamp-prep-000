5be5f9f2ab03dd4cabb43b4f7224b010305379fc
Moufoulyroun Kouferidji <micromouf@yahoo.fr>
2019-02-25 05:43:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     return false 
@@ -6,3 +7,9 @@ def position_taken?(board, index)
   end  
 
 end
+=======
+def #position_taken?(board, index)
+
+end
+
+>>>>>>> e780322701bbdab9bfdfad5e1d28e8c7fcb88db0
